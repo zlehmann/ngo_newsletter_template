7df1b9b91fa760e53b77baa9f26c5a84671f5ba4
Zachary Lehmann <zlehmann@gmail.com>
2019-12-15 17:12:43 -0500

    generated post and event models.

---
+++ backend/db/migrate/20191215214839_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.string :category
+      t.string :post_img
+      t.string :keywords
+      t.timestamps
+    end
+  end
+end
