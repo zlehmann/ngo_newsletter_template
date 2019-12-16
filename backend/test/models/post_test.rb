ad3648bd02efd3fa170bc03b7c78060dcdbf8cd8
Zachary Lehmann <zlehmann@gmail.com>
2019-12-16 10:28:46 -0500

    adding tests for backend.

--- backend/test/models/post_test.rb
+++ backend/test/models/post_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "can create new posts" do
+     post = Post.new
+     assert true
+   end
+
+   test "cannot create post without title" do
+     post = Post.new
+     assert_not(post.valid?)
+   end
 end
