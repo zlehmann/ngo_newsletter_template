70b8678dac8c1b292b6941e8594a048b3abb696f
Zachary Lehmann <zlehmann@gmail.com>
2020-01-26 15:46:56 -0500

    adds index, all, and new methods to post controller.

---
+++ backend/app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    render json: @posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
+
+  def new
+    @post = Post.new
+  end 
+end
