dcb5e5b11b85435932b0ccc05e7ee5d4546d18eb
Zachary Lehmann <zlehmann@gmail.com>
2020-01-27 20:47:40 -0500

    namespace admin and api, add a few seeds for posts.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
-  resources :events
+  namespace :api do
+    resources :posts, only: [:index, :show]
+    resources :events, only: [:index, :show]
+  end
 
-  
+  namespace :admin do
+    resources :posts
+    resources :events
+  end
+
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+posts = Post.create([{title: "Post 1", content: "some content in here", category: "board meeting"},
+                     {title: "Post 2", content: "some other content", category: "member meeting"},
+                     {title: "Post 3", content: "a fishing trip", category: "fishing trip"}])
